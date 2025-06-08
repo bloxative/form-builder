e4d32bd204dcd87844f3042bc8ab8af775a54e88
sothearo-kay <sothearo.kay@techostartup.center>
2025-06-08 15:04:37 +0700

    feat(dev): add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import { createResolver } from "nuxt/kit";
+
+const { resolve } = createResolver(import.meta.url);
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+
+  components: {
+    dirs: [
+      {
+        path: "components",
+        pathPrefix: false,
+      },
+    ],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  css: [resolve("./assets/css/main.css")],
+});
