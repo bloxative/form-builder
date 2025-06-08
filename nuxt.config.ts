c99c43dccb10ba435317f7eae1591bac007737fd
sothearo-kay <sothearo.kay@techostartup.center>
2025-06-08 15:18:27 +0700

    feat(dev): add prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,32 @@
-import tailwindcss from "@tailwindcss/vite";
-import { createResolver } from "nuxt/kit";
+import tailwindcss from '@tailwindcss/vite';
+import { createResolver } from 'nuxt/kit';
 
 const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
   app: {
     head: {
       htmlAttrs: {
-        lang: "en",
-      },
-    },
+        lang: 'en'
+      }
+    }
   },
 
   components: {
     dirs: [
       {
-        path: "components",
-        pathPrefix: false,
-      },
-    ],
+        path: 'components',
+        pathPrefix: false
+      }
+    ]
   },
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
 
-  css: [resolve("./assets/css/main.css")],
+  css: [resolve('./assets/css/main.css')]
 });
