b72af7d2f60f7d757ae515bffb46d0dc83cb7833
sothearo-kay <sothearo.kay@techostartup.center>
2025-06-10 00:56:06 +0700

    feat(example): add contact form schema

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,8 @@
 <script setup lang="ts">
 import Code from '~/components/code.vue';
+import contactFormSchema from '~/example/contact.json';
 
-const formSchema = ref(
-  JSON.stringify(
-    {
-      components: [
-        {
-          name: 'firstName',
-          type: 'text',
-          label: 'Last Name',
-          placeholder: 'Enter your first name',
-          validation: {
-            required: true,
-            minLength: 2
-          },
-          grid: {
-            col: 6
-          }
-        },
-        {
-          name: 'lastName',
-          type: 'text',
-          label: 'Last Name',
-          placeholder: 'Enter your last name',
-          validation: {
-            required: true,
-            minLength: 2
-          },
-          grid: {
-            col: 6
-          }
-        },
-        {
-          name: 'email',
-          type: 'text',
-          inputType: 'email',
-          label: 'Email Address',
-          placeholder: 'your@email.com',
-          validation: {
-            required: true,
-            email: true
-          }
-        }
-      ],
-      gridColumns: 12,
-      gap: '1rem',
-      submitButton: {
-        label: 'Submit',
-        color: 'neutral',
-        variant: 'solid'
-      },
-      cancelButton: {
-        label: 'Cancel',
-        color: 'neutral',
-        variant: 'outline'
-      }
-    },
-    null,
-    2
-  )
-);
+const formSchema = ref(JSON.stringify(contactFormSchema, null, 2));
 
 const { $toast } = useNuxtApp();
 
@@ -86,7 +29,7 @@ function handleFormCancel() {
       <Codemirror v-model="formSchema" class="size-full" />
     </div>
 
-    <div class="sticky top-6 mx-6 max-h-[calc(100vh-6rem)] overflow-auto rounded-lg border">
+    <div class="sticky top-6 mx-6 max-h-[calc(100vh-3rem)] overflow-auto rounded-lg border">
       <div class="border-b bg-neutral-100 px-4 py-2">
         <h3 class="text-sm font-bold">Form Preview</h3>
       </div>
